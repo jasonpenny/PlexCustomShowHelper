d0189c70b870a855587896142e7a6f2b01e7573f
Jason Penny <jasonpenny4@gmail.com>
2016-06-06 22:52:14 -0400

    Create NFO text from parameters

--- plexcustomshowhelper.py
+++ plexcustomshowhelper.py
@@ -1,2 +1,7 @@
-def create_nfo(*args):
-    pass
+def create_nfo(title, season, episode):
+    return '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>\n' \
+            '<episodedetails>\n' \
+            '<title>%s</title>\n' \
+            '<season>%s</season>\n' \
+            '<episode>%s</episode>\n' \
+            '</episodedetails>' % (title, season, episode)
